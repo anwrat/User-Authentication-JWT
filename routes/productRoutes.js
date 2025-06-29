82623164483566f023f75be2be9da59e57689714
anwrat <anweshrwt22@gmail.com>
2025-06-29 12:43:15 +0545

    Delete API using token

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -15,4 +15,18 @@ router.post('/create',requireAuth,async (req,res)=>{
     }
 })
 
+//Delete product
+router.delete('/delete/:productname',requireAuth,async(req,res)=>{
+    try{
+        const {productname} = req.params
+        const deletedProduct = await Products.findOneAndDelete({productname})
+        if(!deletedProduct){
+            return res.status(400).json({error:`The product ${productname} was not found`})
+        }
+        res.status(201).json({message:`The product ${productname} was deleted successfully.`})
+    } catch (err){
+        res.status(400).json({error:'Error in deleting product'})
+    }
+})
+
 module.exports = router
